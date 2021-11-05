4e52c424a3c38b6454535e778900bd2da0511aec
ViniciusLippel <viniciusmlippel@gmail.com>
2021-11-05 09:21:59 -0300

    bfs implemented

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ Created on Wed Nov  3 14:45:20 2021
 @author: Vinícius Lippel
 """
 
-import numpy as np
+from treelib import Tree
+from collections import deque
 import state
 
 
@@ -17,31 +18,60 @@ def print_puz (pos):
 
 #puzzle8 = np.random.choice(9, size=(3,3), replace=False)
 
-puzzle8 = [[3, 2, 4],
-           [5, 0, 1],
+ini_pos = [[3, 2, 4],
+           [5, 1, 0],
            [7, 6, 8]]
 
-empty_pos = [1, 1]
+ini_state = state.state(ini_pos, [1, 2])
 
-state = state.state(puzzle8, empty_pos, 0)
-
-print_puz(state.pos)
-print()
-
-state.left()
-print_puz(state.pos)
-print()
-
-state.up()
-print_puz(state.pos)
-print()
-
-state.right()
-print_puz(state.pos)
-print()
-
-state.down()
-print_puz(state.pos)
-print()
+objective = [[3, 2, 4],
+             [7, 0, 1],
+             [6, 5, 8]]
 
 
+
+# Breadth First Search
+# =============================================================================
+
+def bfs(ini_state):
+    
+    global objective
+    
+    open = deque()
+    closed = []
+    tree = Tree()
+    
+    open.append(ini_state)
+    tree.create_node(ini_state, ''.join(ini_state.path))
+
+    while open:
+        cur_state = open.popleft()
+        closed.append(cur_state.pos)
+        print(cur_state.pos)
+        
+        if cur_state.pos == objective:
+            solved = cur_state
+            return solved
+        
+        for i in cur_state.move():
+            if i:
+                tree.create_node(i, ''.join(i.path), 
+                                 parent=''.join(cur_state.path))
+                if i.pos not in closed:
+                    open.append(i)
+    
+    return solved
+
+# =============================================================================
+
+
+# A* Search
+# =============================================================================
+
+def astar(ini_state):
+    return
+
+# =============================================================================
+
+test = bfs(ini_state)
+print(test.pos, test.path)
--- state.py
+++ state.py
@@ -5,48 +5,79 @@ Created on Wed Nov  3 16:53:01 2021
 @author: vinic
 """
 
+import copy
+
 class state:
     
-    def __init__(self, pos, emp, path):
+    def __init__(self, pos, emp):
         self.pos = pos #positions
         self.emp = emp #position of empty
-        self.path = path
+        self.path = []
 
     
   
     def left(self):
-        s = self
+        s = copy.deepcopy(self)
+        
         if s.emp[1] != 0:
             tile = s.pos[s.emp[0]][s.emp[1]-1]
             s.pos[s.emp[0]][s.emp[1]-1] = 0
             s.pos[s.emp[0]][s.emp[1]] = tile
+            
             s.emp[1] -= 1
+            s.path.append('L')
+            
+            return s
+        
+        return None
+    
     
     def right(self):
-        s = self
+        s = copy.deepcopy(self)
+        
         if s.emp[1] != 2:
             tile = s.pos[s.emp[0]][s.emp[1]+1]
             s.pos[s.emp[0]][s.emp[1]+1] = 0
             s.pos[s.emp[0]][s.emp[1]] = tile
+            
             s.emp[1] += 1
+            s.path.append('R')
+            
+            return s
+        
+        return None
+    
         
     def up(self):
-        s = self
+        s = copy.deepcopy(self)
+        
         if s.emp[0] != 0:
             tile = s.pos[s.emp[0]-1][s.emp[1]]
             s.pos[s.emp[0]-1][s.emp[1]] = 0
             s.pos[s.emp[0]][s.emp[1]] = tile
+            
             s.emp[0] -= 1
-         
+            s.path.append('U')
+            
+            return s
+        
+        return None
         
     def down(self):
-        s = self
+        s = copy.deepcopy(self)
+        
         if s.emp[0] != 2:
             tile = s.pos[s.emp[0]+1][s.emp[1]]
             s.pos[s.emp[0]+1][s.emp[1]] = 0
             s.pos[s.emp[0]][s.emp[1]] = tile
+            
             s.emp[0] += 1
+            s.path.append('D')
+            
+            return s
         
-        
-        
+        return None
+    
+    def move(self):
+        return [self.left(), self.right(), self.up(), self.down()]
         
